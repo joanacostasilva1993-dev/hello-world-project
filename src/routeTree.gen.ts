2882b6d40902155902d8a6e6354a2c111c55198d
joanacostasilva1993-dev <joanacostasilva1993@gmail.com>
2026-09-19 23:58:44 +0200

    Fix generated route type registration for Learning route

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -117,6 +117,7 @@ export interface FileRoutesById {
   '/media': typeof MediaRoute
   '/timeline': typeof TimelineRoute
   '/drift': typeof DriftRoute
+  '/learning': typeof LearningRoute
 }
 
 export interface FileRouteTypes {
